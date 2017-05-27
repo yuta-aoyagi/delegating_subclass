e4e7eb7018de27b450aba9ec5709fd46fdd6180d
Yuta Aoyagi <yuta.aoyagi.1992@gmail.com>
2017-05-27 12:15:04 +0900

    Add some RSpec configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,4 +4,12 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+
+  config.mock_with :rspec do |mocks|
+    mocks.syntax = :expect
+  end
+
+  config.warnings = true
+  config.profile_examples = 10
+  config.order = :random
 end
