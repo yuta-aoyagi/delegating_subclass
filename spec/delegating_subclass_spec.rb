b1e9db0e7216cda406a7e351f1d2f7a49fa5bdf2
Yuta Aoyagi <yuta.aoyagi.1992@gmail.com>
2017-05-30 19:07:14 +0900

    Add the first test

--- spec/delegating_subclass_spec.rb
+++ spec/delegating_subclass_spec.rb
@@ -1,7 +1,22 @@
 require "delegating_subclass"
 
+class WithInitialize
+  def initialize(value)
+    @value = value
+  end
+end
+
 RSpec.describe DelegatingSubclass do
   it "has a version number" do
     expect(DelegatingSubclass::VERSION).not_to be nil
+  end
+
+  describe ".of" do
+    let(:returned_class) { described_class.of WithInitialize, :foo }
+
+    it "returns a class whose ancestors array includes the argument" do
+      pending
+      expect(returned_class.ancestors).to include(WithInitialize)
+    end
   end
 end
